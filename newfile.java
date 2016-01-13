84e3ec3f1a3866f3802f0723d32200054abbbddb
vijith <vvijith@gmail.com>
2016-01-13 15:17:28 +0100

    SIT issue resolved

--- newfile.java
+++ newfile.java
@@ -3,3 +3,5 @@ This is new file java
 Fixing an issue repoorte4d by SIT in Integration branch
 
 Added code for feature 2 in this common code 
+
+Added a bug fix based on SIT defect reported from integration on Feature 2 Sprint 1
